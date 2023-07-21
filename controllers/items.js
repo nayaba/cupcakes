05f7a8ce93bf4f0aba56bd4108c964fed6bbc3cd
Nabila Ayaba <nabila.ayaba@gmail.com>
2023-07-21 18:28:34 -0400

    Create create Item route and controller scaffold

--- controllers/items.js
+++ controllers/items.js
@@ -2,6 +2,11 @@ const newItem = (req, res) => {
   res.render('items/new', {title: 'Create a cupcake'})
 }
 
+const create = (req, res) => {
+  console.log(req.body)
+}
+
 module.exports = {
-  new: newItem
+  new: newItem,
+  create
 }
--- routes/items.js
+++ routes/items.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const itemsCtrl = require('../controllers/items')
 const ensureLoggedIn = require('../config/ensureLoggedIn')
 
+router.post('/', ensureLoggedIn, itemsCtrl.create)
 router.get('/new', ensureLoggedIn, itemsCtrl.new)
 
 module.exports = router
