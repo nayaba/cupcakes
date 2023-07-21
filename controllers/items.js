6d13390547d61358d576bed7b8f650eec119b2ad
Nabila Ayaba <nabila.ayaba@gmail.com>
2023-07-21 18:50:57 -0400

    Create index router and controller

--- controllers/items.js
+++ controllers/items.js
@@ -5,21 +5,26 @@ const newItem = (req, res) => {
 }
 
 const create = async (req, res) => {
-  console.log(req.body)
   if (req.body.allergenFree === 'on') {
-    console.log('in the allergen block')
     req.body.allergenFree = true
   }
   const item = new Item(req.body)
   try {
     await item.save()
-    console.log(item)
+    res.redirect('/items')
   } catch (err) {
     console.log(err.message)
+    res.redirect('/items/new')
   }
+}
+
+const index = async (req, res) => {
+  const items = await Item.find({})
+  res.render('items/index', {title: 'All Items', items})
 }
 
 module.exports = {
   new: newItem,
-  create
+  create,
+  index
 }
--- routes/items.js
+++ routes/items.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const itemsCtrl = require('../controllers/items')
 const ensureLoggedIn = require('../config/ensureLoggedIn')
 
+router.get('/', itemsCtrl.index)
 router.post('/', itemsCtrl.create)
 router.get('/new', itemsCtrl.new)
 
